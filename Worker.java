da5c2ca741859b02a9f6aaafeead8263420e69ff
ArtemEfremov66 <artmasterae@mail.ru>
2024-06-17 20:42:47 +0300

    Add files via upload

---
+++ Worker.java
@@ -0,0 +1,13 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 10; i++) {
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
